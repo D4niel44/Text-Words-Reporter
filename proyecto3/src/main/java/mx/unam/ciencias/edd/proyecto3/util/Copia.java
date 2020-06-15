fd153057aa62b8d8a4eefbb4d9ef952345f133a9
Daniel Linares Gil <daniel44@main-pc.myhost>
2020-06-15 12:15:47 -0500

    clase auxiliar para copiar datos entre flujos

---
+++ proyecto3/src/main/java/mx/unam/ciencias/edd/proyecto3/util/Copia.java
@@ -0,0 +1,25 @@
+package mx.unam.ciencias.edd.proyecto3.util;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+/**
+ * Clase que ofrece métodos estáticos para copiar archivos.
+ */
+public class Copia {
+    
+    private Copia() {}
+
+    /**
+     * Escribe los contenidos del stream de entrada en el de salida y cierra ambos.
+     * @param entrada Stream de los datos a copiar.
+     * @param salida Stream en el cual copiar los datos.
+     * @throws IOException Si ocurre un error de I/O.
+     */
+    public static void copia(InputStream entrada, OutputStream salida) throws IOException {
+        try (InputStream in = entrada;  OutputStream out = salida) {
+            out.write(in.read());
+        }
+    }
+}
