5ff269556300b2c5df73dafe6905c8d0b1da8d31
Daniel Linares Gil <daniel44@main-pc.myhost>
2020-06-18 20:17:37 -0500

    cambios App

--- proyecto3/src/main/java/mx/unam/ciencias/edd/proyecto3/App.java
+++ proyecto3/src/main/java/mx/unam/ciencias/edd/proyecto3/App.java
@@ -1,16 +1,18 @@
 package mx.unam.ciencias.edd.proyecto3;
 
 /**
- * Contiene el método main y se encarga de imprimir en pantalla el resultado y*los errores.
+ * Contiene el método main y se encarga de imprimir en pantalla el resultado y
+ * los errores.
  */
 public class App {
+
 	/**
-	 * Método principal. Recibe los argumentos introducidos por el usuario crea. Ejecuta el
-	 * programa.
+	 * Método principal. Recibe los argumentos introducidos por el usuario y ejecuta
+	 * el programa.
 	 */
 	public static void main(String[] args) {
 		try {
-			Reporte app = new Reporte(args);
+			Reportador app = new Reportador(args);
 			app.ejecutar();
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
