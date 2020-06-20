485b4154540f7f32ae3bb6f1610d92c007867ebb
Daniel Linares Gil <daniel44@main-pc.myhost>
2020-06-19 21:32:58 -0500

    docu excepciones

--- proyecto3/src/main/java/mx/unam/ciencias/edd/proyecto3/excepciones/ExcepcionArchivoNocreado.java
+++ proyecto3/src/main/java/mx/unam/ciencias/edd/proyecto3/excepciones/ExcepcionArchivoNocreado.java
@@ -4,15 +4,29 @@ package mx.unam.ciencias.edd.proyecto3.excepciones;
  * Se lanza cuando ocurre un error al crear un archivo.
  */
 public class ExcepcionArchivoNocreado extends RuntimeException {
-    
+
+    /**
+     * Constructor por omisión.
+     */
     public ExcepcionArchivoNocreado() {
         super();
     }
 
+    /**
+     * Constructor que recibe un mensaje.
+     * 
+     * @param mensaje Mensaje.
+     */
     public ExcepcionArchivoNocreado(String mensaje) {
         super(mensaje);
     }
 
+    /**
+     * Constructor que recibe un mensaje y una causa.
+     * 
+     * @param mensaje Mensaje .
+     * @param c       Causa de la excepción.
+     */
     public ExcepcionArchivoNocreado(String mensaje, Throwable c) {
         super(mensaje, c);
     }
